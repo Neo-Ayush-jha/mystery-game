6c1c40de1f062067e98c0bd2990ad4cb44360ee1
Neo-Ayush-jha <ayush91176@gmail.com>
2025-04-08 12:27:13 +0530

    day 6

--- nyaydetect/settings.py
+++ nyaydetect/settings.py
@@ -124,7 +124,7 @@ DATABASES = {
 }
 
 
-# DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
+DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
